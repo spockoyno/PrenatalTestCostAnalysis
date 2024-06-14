af50f70c992837d76992df7bacea9897a3c7c032
spockoyno <volkov.rrm@gmail.com>
2024-06-15 00:38:54 +0100

    Doing ng-containers displays for App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButton} from "@angular/material/button";
 
@@ -11,16 +11,27 @@ import {PriceScenarioPlotComponent} from "./price-scenario-plot/price-scenario-p
 import {SimulationInputsComponent} from "./simulation-inputs/simulation-inputs.component";
 import {SimulationOutputComponent} from "./simulation-output/simulation-output.component";
 import {MatDivider} from "@angular/material/divider";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton, AsyncPipe, InputsComponent, OutputsComponent, PriceScenarioPlotComponent, SimulationInputsComponent, SimulationOutputComponent, PriceScenarioPlotComponent, MatDivider],
+  imports: [RouterOutlet, MatButton, AsyncPipe, InputsComponent, OutputsComponent, PriceScenarioPlotComponent, SimulationInputsComponent, SimulationOutputComponent, PriceScenarioPlotComponent, MatDivider, MatButtonToggleGroup, MatButtonToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup = this.fb.group({
+    // Setting both toggle values to true
+    switches: this.fb.control(['sensitivity', 'simulation'])
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+
+  }
 
 
 
